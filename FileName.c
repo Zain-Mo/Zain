9fc366bcdec9ebd3fc3ef51898b933dfd1029ac7
Zain-Mo <zaynawad2@gmail.com>
2026-01-16 11:27:20 -0500

    Add files via upload

---
+++ FileName.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+
+	int x = 17, y = 10, rem , result;
+	float z = 5.66;
+	double output;
+
+	rem, x % y;  //reminder operator
+
+	//narowwing
+	result = z + 0.44;
+
+	//promotion
+	output = x - 7;
+
+	double result2 = x / (double)y;
+	printf("Result2 = %If\n , result2");
+
+	//mins to hours
+	printf("Mins: %d to Hrs: %If\n" , 45 , 45 / 60.0);
+
+
+
+	return 0;
+}
---
+++ lab1a.c
@@ -0,0 +1,31 @@
+/***************************************************************** 
+
+    File: lab1a.c
+    
+    Author: [Zain Mohamed]
+    Seneca email: [zmohamed25@myseneca.ca]
+
+    To compile program in codespaces, in terminal pane type:
+        gcc -Wall lab1a.c
+    To run program in codespaces, in terminal pane type:
+        ./a.out
+        
+***************************************************************/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+
+    int celsius, faranhite
+        char = 2 , 4
+        float;
+    double numone, numtwo, res;
+    printf("Enter a number for celsius");
+    scanf("%Lf", &numone);
+    printf("Enter a number for faranhite");
+    scanf("%Lf", &numtwo);
+
+
+    return 0;
+}
+
